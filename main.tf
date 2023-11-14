41305182ddbaddb493b5ea4a6259d2a3d573b322
vinaynasani <51323278+vinaynasani@users.noreply.github.com>
2023-11-14 11:28:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ca-central-1"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  count=4
+  tags = {
+    Name = "instance-${count.index}",
+    role=count.index==0?"lb": (count.index<3?"web":"backend")
+  }
+}
+
+output "ips"{
+  value = aws_instance.instance.*.public_ip
+}
