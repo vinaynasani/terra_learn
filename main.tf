c370241fd2e25e4f66ca7e2f06cd0ca54f6d977f
vinaynasani <51323278+vinaynasani@users.noreply.github.com>
2023-11-15 10:23:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  key_name="user7_deployer-key"
   count=4
   tags = {
     Name = "instance-${count.index}",
